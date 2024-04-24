6fef831841a52f13c96eae378e2c9a489be8a370
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-04-23 20:01:29 -0600

    Feat: add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 
 export default function Main() {
   return (
-    <h1 className='text-3xl text-red-800'>
-      main
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/projects" element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
