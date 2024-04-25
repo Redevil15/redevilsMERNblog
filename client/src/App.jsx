6ef9decdb641ff8f76b8db503ceb8ba80fd0c635
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-04-25 17:20:45 -0600

    Feat: add footer page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 export default function Main() {
@@ -21,6 +22,7 @@ export default function Main() {
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/projects" element={<Projects/>} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
