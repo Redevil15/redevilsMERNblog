a36633b0754f698455748e27772b2ad15f396935
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-04-26 17:46:00 -0600

    Feat: complete dark mode functionality using redux

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,18 @@
-import { Avatar, Button, Dropdown, DropdownDivider, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import { FaMoon, FaSun } from 'react-icons/fa'
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 export default function Header() {
 
   const path = useLocation().pathname;
   const currentUser = useSelector(state => state.user.currentUser);
-  console.log('Usuario', currentUser)
+  const dispatch = useDispatch();
+  const actualTheme = useSelector(state => state.theme.theme);
 
   return (
     <Navbar className='border-b-2'>
@@ -36,8 +39,13 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon/>
+        <Button 
+          className='w-12 h-10 hidden sm:inline' 
+          color='gray' 
+          pill
+          onClick={() => dispatch(toggleTheme())}  
+        >
+          {actualTheme === 'dark' ? <FaSun/> : <FaMoon/>}
         </Button>
         {currentUser ? (
           <Dropdown
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/themeProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
       <React.StrictMode>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </React.StrictMode>,
     </Provider>
   </PersistGate>
