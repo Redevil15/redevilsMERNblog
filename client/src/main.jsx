73a3a7152f3c2e44e5866f7a8c6e8ac861148889
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-04-25 20:14:51 -0600

    Feat: add redux persist functionality to keep the data in localStorage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    </Provider>
+  </PersistGate>
 )
