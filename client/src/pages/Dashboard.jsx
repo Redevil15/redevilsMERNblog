fde71989f9a6f6d057d1f8dbc9ef2b33c322bd56
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-04-29 11:17:29 -0600

    Feat: complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/dashSidebar";
+import DashProfile from "../components/dashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('')
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location.search]);
   return (
-    <div>
-      dashboard page
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar/>
+      </div>
+      {tab === 'profile' && <DashProfile/>}
     </div>
   )
 }
