de04c75561c635c85da83bee785b2985ba475311
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-05-08 11:24:50 -0600

    Feat: add delete user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -46,4 +46,18 @@ export const updateUser = async (req, res, next) => {
     } catch (error) {
       next(error)
     }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if(req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You're not allowe to delete this user"))
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+
+    res.status(200).json({ message: "User deleted successfully!!" });
+  } catch (error) {
+    next(error)
+  }
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put('/update/:userId', verifyUser, updateUser)
+router.delete('/delete/:userId', verifyUser, deleteUser)
 
 export default router;
