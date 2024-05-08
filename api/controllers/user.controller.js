d8349bba893251d4b512f74a1db7c771ac05c7d6
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-05-08 11:46:22 -0600

    Feat: add signout user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -60,4 +60,15 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+};
+
+export const signout = async (req, res, next) => {
+  try {
+    res
+      .clearCookie('access_token')
+      .status(200)
+      .json({ message: "User signed out successfully!!" });
+  } catch (error) {
+    next(error)
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, signout, test, updateUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/test", test);
 router.put('/update/:userId', verifyUser, updateUser)
 router.delete('/delete/:userId', verifyUser, deleteUser)
+router.post('/signout', signout)
 
 export default router;
