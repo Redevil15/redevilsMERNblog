7095074e7803fbef1bc32a5b7c344f03d39534d4
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-05-10 15:29:39 -0600

    Add getposts API route and controller function

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, create)
+router.get('/getposts', getposts)
 
 export default router;
