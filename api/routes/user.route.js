d44f7aabe968abf139476f98d8300c39d7063ad2
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-05-13 15:18:15 -0600

    feat: Add getusers API endpoint to retrieve all users

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, signout, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getusers, signout, test, updateUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get("/test", test);
 router.put('/update/:userId', verifyUser, updateUser)
 router.delete('/delete/:userId', verifyUser, deleteUser)
 router.post('/signout', signout)
+router.get('/getusers', verifyUser, getusers)
 
 export default router;
