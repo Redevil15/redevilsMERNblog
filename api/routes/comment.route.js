ca441b9d051530638bfb7fded78cb04b0ae9f58c
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-05-15 16:24:25 -0600

    feat: Implement like functionality for comments API

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/create', verifyUser, createComment)
 router.get('/getComments/:postId', getComments)
+router.put('/likeComment/:commentId', verifyUser, likeComment)
 
 export default router;
