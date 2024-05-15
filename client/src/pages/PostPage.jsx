b475511557672f9b67f30dc9a8d3a190120af19b
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-05-15 12:54:48 -0600

    feat: Add CallToAction component to PostPage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
   const { postSlug } = useParams()
@@ -67,6 +68,9 @@ export default function PostPage() {
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       >
 
+      </div>
+      <div className='max-w-4xl mx-auto w-full post-content'>
+        <CallToAction/>
       </div>
     </main>
   )
